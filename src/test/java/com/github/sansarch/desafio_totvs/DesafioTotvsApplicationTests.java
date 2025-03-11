857ba40c01a8949d4053c28fb14fa282b769c05f
Santiago Negreira <sansfleitas@gmail.com>
2025-03-11 20:29:31 -0300

    Initialize project

---
+++ src/test/java/com/github/sansarch/desafio_totvs/DesafioTotvsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sansarch.desafio_totvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTotvsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
