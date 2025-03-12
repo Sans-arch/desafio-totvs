d9d258c34cd24e68d658ef1f557799805cfa2283
Santiago Negreira <sansfleitas@gmail.com>
2025-03-12 02:10:33 -0300

    Create PayableException

---
+++ src/main/java/com/github/sansarch/desafio_totvs/domain/exception/PayableException.java
@@ -0,0 +1,7 @@
+package com.github.sansarch.desafio_totvs.domain.exception;
+
+public class PayableException extends RuntimeException {
+    public PayableException(String message) {
+        super(message);
+    }
+}
