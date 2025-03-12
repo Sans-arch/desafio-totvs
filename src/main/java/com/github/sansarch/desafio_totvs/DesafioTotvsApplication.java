69ebd25a5c6cea23419db6b4a9bc0f737d85baff
Santiago Negreira <sansfleitas@gmail.com>
2025-03-12 16:57:49 -0300

    Add spring data web support (to enhance JSON page dto serialization)

--- src/main/java/com/github/sansarch/desafio_totvs/DesafioTotvsApplication.java
+++ src/main/java/com/github/sansarch/desafio_totvs/DesafioTotvsApplication.java
@@ -2,7 +2,9 @@ package com.github.sansarch.desafio_totvs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class DesafioTotvsApplication {
 
