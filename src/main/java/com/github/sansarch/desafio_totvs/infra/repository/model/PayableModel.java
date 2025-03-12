ea41e634326950568209c2cf7cd62fa8f2dfbe26
Santiago Negreira <sansfleitas@gmail.com>
2025-03-12 02:18:54 -0300

    Create Payables table (using FlyWay) and model (JPA)

---
+++ src/main/java/com/github/sansarch/desafio_totvs/infra/repository/model/PayableModel.java
@@ -0,0 +1,31 @@
+package com.github.sansarch.desafio_totvs.infra.repository.model;
+
+import com.github.sansarch.desafio_totvs.domain.entity.PayableStatus;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payables")
+public class PayableModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private LocalDateTime dueDate;
+
+    @Column
+    private LocalDateTime paymentDate;
+
+    @Column
+    private BigDecimal value;
+
+    @Column
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private PayableStatus status;
+}
